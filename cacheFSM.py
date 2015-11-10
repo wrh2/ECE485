7af71e23c8eebf8a670b9c955336ef53288c7cb8
wrh2 <wrh2@pdx.edu>
2015-11-10 01:06:44 -0800

    modified the cacheFSM, put random numbers in memory, did some reading/writing, generated vcd and pdf of result

--- cacheFSM.py
+++ cacheFSM.py
@@ -9,7 +9,7 @@ from random import randrange
 # declare states
 t_State = enum('WAIT', 'READ', 'WRITE')
 
-def cacheFSM(r, w, clk, state):
+def cacheFSM(addr, r, w, din, dout, clk, state):
     """
     Function contains finite state machine definition
 
@@ -19,6 +19,14 @@ def cacheFSM(r, w, clk, state):
     :param state: current state
     """
 
+    # not sure what intbv really is or why the [8:] is necessary
+    #mem = [Signal(0)[8:]) for i in range(128)]
+
+    # lets try this, assign random ints to memory just to fill it up
+    # data bus width is 8 bits so nothing over 255
+    # and lets make the memory have 256 slots because I'm just testing right now
+    mem = [Signal(randrange(255)) for i in range(256)]
+
     @always(clk.posedge)
     def logic():
 
@@ -27,12 +35,14 @@ def cacheFSM(r, w, clk, state):
 
             # next state is read
             state.next = t_State.READ
+            dout.next = mem[addr]
 
         # write is high, read is low
         elif w == 1 and r == 0:
 
             # next state is write
             state.next = t_State.WRITE
+            mem[addr].next = din
 
         # both high or both low
         else:
@@ -50,11 +60,14 @@ def testbench():
 
     # initialize signals
     r, w, clk = [Signal(bool(0)) for i in range(3)]
+    dout = Signal(0)
+    addr = Signal(0)
+    din = Signal(0)
     state = Signal(t_State.WAIT) # default state
 
     # instance of cacheFSM
-    #cache_inst = cacheFSM(r, w, clk, state)
-    cache_inst = toVerilog(cacheFSM, r, w, clk, state)
+    #cache_inst = cacheFSM(addr, r, w, din, dout, clk, state)
+    cache_inst = toVerilog(cacheFSM, addr, r, w, din, dout, clk, state)
 
     # clock generation
     @always(delay(10))
@@ -66,17 +79,18 @@ def testbench():
     def stimulus():
         r.next = randrange(2)
         w.next = randrange(2)
-
+        addr.next = randrange(255)
+        din.next = randrange(255)
 
     # print the output, just for fun
     @instance
     def output_monitor():
-        print "r w clk state"
-        print "-------------"
+        print "r w addr din dout clk state"
+        print "---------------------------"
         while True:
             yield clk.posedge
             yield delay(1)
-            print "%d %d" % (r, w),
+            print "%d %d %d %d %d" % (r, w, addr, din, dout),
             yield clk.posedge
             print "C",
             yield delay(1)
